86f1b595a1099c7cbd36c64e52a236037c73d3da
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-07-28 14:01:01 +0300

    general

--- src/modules/notification/dto/distance.dto.ts
+++ src/modules/notification/dto/distance.dto.ts
@@ -1,14 +1,18 @@
 import { IsNotEmpty, IsString, IsLatitude } from 'class-validator';
+import { Transform, TransformFnParams } from 'class-transformer';
 
 export class DistanceDto {
+  @Transform(({ value }: TransformFnParams) => value.trim())
   @IsLatitude()
   @IsNotEmpty()
   latitude: number;
 
+  @Transform(({ value }: TransformFnParams) => value.trim())
   @IsLatitude()
   @IsNotEmpty()
   longitude: number;
 
+  @Transform(({ value }: TransformFnParams) => value.trim())
   @IsString()
   @IsNotEmpty()
   orderId: string;
